d23f40c268cee2ad72d8ef6d130a70c6b031eae0
bangnokia <bangnokia@gmail.com>
2024-04-16 00:58:00 +0700

    refactor to value swatch

--- src/components/Option.vue
+++ src/components/Option.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import {computed, defineProps} from 'vue';
 import {Option} from '../lib/types';
-import {variants, form} from "../store";
-import {TooltipArrow, TooltipContent, TooltipProvider, TooltipRoot, TooltipTrigger} from 'radix-vue'
+import {form, variants} from "../store";
+import {TooltipArrow, TooltipContent, TooltipRoot, TooltipTrigger} from 'radix-vue'
+import ValueSwatch from "./ValueSwatch.vue";
 
 const {option, dependsOn} = defineProps<{
   option: Option,
@@ -73,8 +74,7 @@ const values = computed(() => {
                      required
                      style="display: none">
 
-              <div v-if="!value.color" class="basic-value" v-text="value.name"/>
-              <div v-else class="color-swatch" :style="{ background: value.color }"/>
+              <ValueSwatch :value="value"/>
             </label>
           </TooltipTrigger>
           <TooltipContent style="padding: 4px 8px; color: white; font-size: 0.875rem; background: rgba(0, 0, 0,  0.9); border-radius: 4px;">
@@ -103,34 +103,4 @@ const values = computed(() => {
   flex-wrap: wrap;
 }
 
-.basic-value {
-  font-size: 0.85rem;
-  border: 1px solid #e3e3e3;
-  border-radius: 0.5rem;
-  padding: 0.25rem 0.5rem;
-  cursor: pointer;
-  min-width: 2rem;
-  text-align: center;
-}
-
-input:checked + .basic-value {
-  background-color: #16a34a;
-  color: #fff;
-  outline: 1px solid #16a34a;
-}
-
-.color-swatch {
-  width: 2rem;
-  height: 2rem;
-  padding: 0;
-  border: 1px solid #e3e3e3;
-  border-radius: 100%;
-  cursor: pointer;
-}
-
-input:checked + .color-swatch {
-  outline: 2px solid #16a34a;
-  outline-offset: 2px;
-  border-color: transparent;
-}
 </style>
---
+++ src/components/ValueSwatch.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import {type Value} from "../lib/types.ts";
+
+const {value} = defineProps<{
+  value: Value
+}>()
+</script>
+
+<template>
+  <div v-if="!value.color" class="basic-value" v-text="value.name"/>
+  <div v-else class="color-swatch" :style="{ background: value.color }"/>
+</template>
+<style>
+
+.basic-value {
+  font-size: 0.85rem;
+  border: 1px solid #e3e3e3;
+  border-radius: 0.5rem;
+  padding: 0.25rem 0.5rem;
+  cursor: pointer;
+  min-width: 2rem;
+  text-align: center;
+}
+
+input:checked + .basic-value {
+  background-color: #16a34a;
+  color: #fff;
+  outline: 1px solid #16a34a;
+}
+
+.color-swatch {
+  width: 2rem;
+  height: 2rem;
+  padding: 0;
+  border: 1px solid #e3e3e3;
+  border-radius: 100%;
+  cursor: pointer;
+}
+
+input:checked + .color-swatch {
+  outline: 2px solid #16a34a;
+  outline-offset: 2px;
+  border-color: transparent;
+}
+</style>
